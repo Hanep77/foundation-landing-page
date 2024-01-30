45a8795b657604fcad81cc263429ba92a51712e4
hanep <yudishan26@gmail.com>
2024-01-30 23:58:37 +0700

    added "go down" button

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,20 @@ export default function Home() {
             <div className="w-5/12">
               <h1 className="text-6xl font-bold text-cyan-800"><span className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-blue-800 to-orange-600">Knowledge</span> is power; <span className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-blue-800 to-orange-600">education</span> is its source.</h1>
               <p className="tracking-widest text-lg text-slate-700 mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-              <div>
-                <div className="flex flex-col gap-2 mt-5">
-                  <div className="w-2 h-2 rounded-full bg-blue-800"></div>
+              <div className="flex mt-8 gap-8">
+                <div className="flex flex-col gap-2">
+                  <div className="w-2 h-2 rounded-full bg-blue-900"></div>
+                  <div className="w-2 h-2 rounded-full bg-blue-600"></div>
                   <div className="w-2 h-2 rounded-full bg-blue-400"></div>
                   <div className="w-2 h-2 rounded-full bg-red-500"></div>
+                  <div className="w-2 h-2 rounded-full bg-orange-500"></div>
                   <div className="w-2 h-2 rounded-full bg-yellow-500"></div>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <Link href="#about" className="w-16 h-16 flex justify-center items-center bg-cyan-800 rounded-full animate-bounce">
+                    <img src="/icons/down-lg.svg" className="w-6" />
+                  </Link>
+                  <p className="tracking-wider text-xs text-slate-600">Let's Discover</p>
                 </div>
               </div>
               {/* <Link href="#about">Discover more</Link> */}
@@ -48,7 +56,7 @@ export default function Home() {
                 <div className="flex gap-2 mt-5 absolute z-10 -top-14 right-0">
                   <div className="w-2 h-2 rounded-full bg-blue-900"></div>
                   <div className="w-2 h-2 rounded-full bg-blue-600"></div>
-                  <div className="w-2 h-2 rounded-full bg-blue-300"></div>
+                  <div className="w-2 h-2 rounded-full bg-blue-400"></div>
                   <div className="w-2 h-2 rounded-full bg-red-500"></div>
                   <div className="w-2 h-2 rounded-full bg-orange-500"></div>
                   <div className="w-2 h-2 rounded-full bg-yellow-500"></div>
